2fd093c8a1e91fe2deb858734fc3f76eef1820f6
Sean Stanfield <seanstanfield0@gmail.com>
2020-06-16 12:02:02 +0100

    attempted to add autoprefixer with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,15 @@ module.exports = {
     path: __dirname + "/dist",
     filename: "bundle.js",
   },
+  rules: [
+    {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    },
+  ],
 };
