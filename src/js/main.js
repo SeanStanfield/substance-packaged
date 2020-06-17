7d4eedecec40b338ad1f2464c6963112b41ff6dc
Sean Stanfield <seanstanfield0@gmail.com>
2020-06-17 15:31:57 +0100

    added services copy and graph shell

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
+import Scrollbar from "smooth-scrollbar";
+
+// let body = document.querySelector("#mainScroll");
+
+// Scrollbar.init(body);
+
 console.log("log from the mainjs file");
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,4 @@ module.exports = {
     path: __dirname + "/dist",
     filename: "bundle.js",
   },
-  rules: [
-    {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
-    },
-    {
-      test: /\.css$/,
-      use: ["style-loader", "css-loader", "postcss-loader"],
-    },
-  ],
 };
