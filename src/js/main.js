e588e666782d6be65b9a0d5e4da6c37dc3c767c6
Sean Stanfield <seanstanfield0@gmail.com>
2020-06-23 15:08:50 +0100

    added floating info box

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,48 @@
 import Scrollbar from "smooth-scrollbar";
 
-// let body = document.querySelector("#mainScroll");
+let cssPopup = document.querySelector(".popup");
 
-// Scrollbar.init(body);
+const graphBlobs = document.querySelectorAll(".graphBlob");
+const desktopGraph = document.querySelector(".graph");
 
-const arrow = document.querySelector("#arrow");
-console.log(arrow);
+const popupObject = {
+  Innovation: {
+    title: "3 word title",
+    points: ["Touch, Smell, Sight, Sound", "Real Touchy Feely Stuff"],
+    caseStudies: ["Tacticle Dynamics"],
+  },
 
-document.addEventListener("click", (e) => {
-  console.log(e.target);
+  Branding: {
+    title: "New branding title",
+    points: ["Brandig point 1", "Branding point 2", "Branding point 3"],
+    caseStudies: ["Branding Case Study"],
+  },
+};
+
+function changePopupContents(Blob) {
+  let BlobId = Blob.id;
+  console.log(cssPopup.children[0].innerHTML, BlobId);
+  let newTitle = popupObject[BlobId].title;
+  cssPopup.children[0].innerHTML = newTitle;
+}
+
+graphBlobs.forEach((blob) => {
+  blob.addEventListener("mouseover", (e) => {
+    // let popup = createPopup(blob);
+    // console.log(popup);
+    // desktopGraph.appendChild(popup);
+    cssPopup.style.top = `${blob.getAttribute("y")}`;
+    cssPopup.style.left = `${blob.getAttribute("x")}`;
+    changePopupContents(blob);
+    cssPopup.classList.remove("fade-out");
+  });
+
+  blob.addEventListener("mouseleave", () => {
+    cssPopup.classList.add("fade-out");
+  });
 });
 
-arrow.addEventListener("click", () => {
-  console.log("arrowPressed");
-});
+// const scream = document.createElement("h1");
+// scream.innerText = "PLEASE BE ON TOP";
+
+// desktopGraph.appendChild(scream);
