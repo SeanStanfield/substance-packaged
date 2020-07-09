d9da7b43b63cddc10235f51372087a215921855d
Sean Stanfield <seanstanfield0@gmail.com>
2020-07-09 16:42:24 +0100

    code to change contents based on json

--- src/js/adaptive.js
+++ src/js/adaptive.js
@@ -1,13 +1,35 @@
 const navLinks = document.querySelectorAll(".link");
+let mainContent = document.querySelector("#mainContent");
 console.log(navLinks);
 
-let fs = require("fs");
-let data = {};
+// let fs = require("fs");
+// let data = {};
 
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    let clickedOn = link.getAttribute("data-url");
-    data.clicked = clickedOn;
-    console.log(data);
-  });
-});
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     let clickedOn = link.getAttribute("data-url");
+//     data.clicked = clickedOn;
+//     console.log(data);
+//   });
+// });
+
+let requestURL =
+  "https://raw.githubusercontent.com/SeanStanfield/substance-packaged/master/caseStudy.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
+request.onload = function () {
+  const jsonResponse = request.response;
+  populateHeader(jsonResponse);
+};
+
+function populateHeader(jsonObj) {
+  let studyHeader = document.querySelector("#studyTitle");
+  studyHeader.textContent = jsonObj["title"];
+  mainContent.appendChild(studyHeader);
+  let studyBody = document.querySelector("#studyBody");
+  studyBody.textContent = jsonObj["content"];
+  mainContent.appendChild(studyBody);
+}
